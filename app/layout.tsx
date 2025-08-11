abc9ba8af9696dd3a1dc141fd0095e7a70dac1bd
Bilal Jadi <148371977+B-JCoder@users.noreply.github.com>
2025-08-11 11:00:38 -0700

    book list icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,19 +6,19 @@ import './globals.css'
 export const metadata: Metadata = {
   title: "JAY-S EZ JUNK REMOVAL | Junk Removal Services USA & GTA",
   description: "Professional junk removal services in the Greater Toronto Area and across the USA. Affordable, eco-friendly, and same-day pickup available. Call now!",
-  keywords: [
-    "junk removal USA",
-    "junk cleaning service",
-    "trash removal",
-    "waste disposal",
-    "junk removal GTA",
-    "affordable junk hauling",
-    "eco-friendly junk removal",
-    "same-day junk pickup",
-    "clean out services USA",
-    "residential junk removal",
-    "commercial junk hauling"
-  ],
+  // keywords: [
+  //   "junk removal USA",
+  //   "junk cleaning service",
+  //   "trash removal",
+  //   "waste disposal",
+  //   "junk removal GTA",
+  //   "affordable junk hauling",
+  //   "eco-friendly junk removal",
+  //   "same-day junk pickup",
+  //   "clean out services USA",
+  //   "residential junk removal",
+  //   "commercial junk hauling"
+  // ],
   openGraph: {
     title: "JAY-S EZ JUNK REMOVAL",
     description: "Expert junk removal services across USA and GTA. Call 214-258-3511 for fast, affordable cleanup.",
@@ -26,7 +26,7 @@ export const metadata: Metadata = {
     siteName: "JAY-S EZ JUNK REMOVAL",
     images: [
       {
-        url: "/images/jay_slogo--removebg-preview.png",
+        url: "/images/jay's.png",
         width: 600,
         height: 400,
         alt: "JAY-S EZ Junk Removal Logo"
